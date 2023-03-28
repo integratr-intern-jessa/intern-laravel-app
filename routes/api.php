e278a197ee3d002de3ca11d959cc4e0ba44cb7ff
Jessa Mae Aruta <jaruta.intern.integratr@outlook.com>
2023-03-28 18:42:19 +0800

    updated

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\PersonController;
+use App\Http\Controllers\ProductController;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,11 +17,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+// Persons
 Route::get('/persons', [PersonController::class, 'index']);
 Route::post('/persons', [PersonController::class, 'store']);
 Route::put('/persons/{id}', [PersonController::class, 'update']);
 Route::get('/persons/{id}', [PersonController::class, 'show']);
 Route::delete('/persons/{id}', [PersonController::class, 'destroy']);
+
+// Products
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
 
 
